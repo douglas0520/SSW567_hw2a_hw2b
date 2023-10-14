20ddef7835b41671bb443c5aa1cbe261dc22f821
douglas0520 <145615003+douglas0520@users.noreply.github.com>
2023-10-14 18:45:04 -0400

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,51 +1,97 @@
-# -*- coding: utf-8 -*-
-"""
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
-
-@author: jrr
-@author: rk
-"""
-
 import unittest
-
-from Triangle import classifyTriangle
+from classify_triangle import classify_triangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
-class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+class ClassifyingTriangle(unittest.TestCase):
+    """ClassifyingTriangles : list of test cases for various inputs in the
+    classify triangle program"""
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-    
-    def testIsosceles(self):
-        self.assertEqual(classifyTriangle(3,3,4), 'Isosceles', '3,3,4 is an isosceles triangle')
+    def test_right_triangle_a(self):
+        """test_right_triangle_a : test case for a right triangle
+        returns : Triangle_Is_Right, "3,4,5 is a Right triangle"""
+        self.assertEqual(
+            classify_triangle(3, 4, 5), "Triangle_Is_Right", "3,4,5 is a Right triangle"
+        )
 
-    def testScalene(self):
-        self.assertEqual(classifyTriangle(5,7,9), 'Scalene', '5,7,9 is an scalene triangle')
+    def test_right_triangle_b(self):
+        """test_right_triangle_b : test case for a right triangle
+        returns : Triangle_Is_Right, "5,3,4 is a Right triangle"""
 
-    def testInputLimitMax(self):
-        self.assertEqual(classifyTriangle(300,78,360), 'InvalidInput_Reached_Limit', 'Input_Limit_Reached (> 200)')
-    
-    def testInputLimitMin(self):
-        self.assertEqual(classifyTriangle(-10,10,10), 'InvalidInput_Reached_Limit', 'Input_Limit_Reached (< 0)')
+        self.assertEqual(
+            classify_triangle(5, 3, 4), "Triangle_Is_Right", "5,3,4 is a Right triangle"
+        )
 
-    def testIsATriangle(self):
-        self.assertEqual(classifyTriangle(1,10,23), 'InvalidInput_Not_A_Triangle', '1,10,23 is not a triangle')
+    def test_equilateral_triangles(self):
+        """test_equilateral_triangles :test case for equilateral triangles
+        returns: Triangle_Is_Equilateral, 1,1,1 should be equilateral"""
+        self.assertEqual(
+            classify_triangle(1, 1, 1),
+            "Triangle_Is_Equilateral",
+            "1,1,1 should be equilateral",
+        )
 
-    def testFloatNumbers(self):
-        self.assertEqual(classifyTriangle(1.1,2.2,3.3), 'Invalid_Input_Using_Float', 'Input = Float')
+    def test_isosceles(self):
+        """test_isosceles : test case for isosceles triangles
+        returns: Triangle_Is_Isosceles, 3,3,4 is an isosceles triangle"""
+        self.assertEqual(
+            classify_triangle(3, 3, 4),
+            "Triangle_Is_Isosceles",
+            "3,3,4 is an isosceles triangle",
+        )
 
-if __name__ == '__main__':
-    print('Running unit tests')
+    def test_scalene(self):
+        """test_scalene : test case for scalene triangles
+        returns : Triangle_Is_Scalene, 5,7,9 is an scalene triangle"""
+        self.assertEqual(
+            classify_triangle(5, 7, 9),
+            "Triangle_Is_Scalene",
+            "5,7,9 is an scalene triangle",
+        )
+
+    def test_input_limit_max(self):
+        """test_input_limit_max : test if the input is beyond the preset max amount
+        returns : Input_limit  "Invalid_Input_Reached_Limit",
+            "Input_Limit_Reached (> 200)","""
+        self.assertEqual(
+            classify_triangle(300, 78, 360),
+            "Invalid_Input_Reached_Limit",
+            "Input_Limit_Reached (> 200)",
+        )
+
+    def test_input_limit_min(self):
+        """test_input_limit_min : test if the input is beyond the preset min amount
+        returns : Input_limit  "Invalid_Input_Reached_Limit",
+            "Input_Limit_Reached (< 0)","""
+        self.assertEqual(
+            classify_triangle(-10, 10, 10),
+            "Invalid_Input_Reached_Limit",
+            "Input_Limit_Reached (< 0)",
+        )
+
+    def test_is_a_triangle(self):
+        """test_is_a_triangle : tests if the inputs correlate to an actual triangle
+        returns: "Invalid_Input_Not_A_Triangle",
+            "1,10,23 is not a triangle" """
+        self.assertEqual(
+            classify_triangle(1, 10, 23),
+            "Invalid_Input_Not_A_Triangle",
+            "1,10,23 is not a triangle",
+        )
+
+    def test_float_numbers(self):
+        """test_float_number : tests is the input used is a float, the program only takes in integer inputs
+        returns : "Invalid_Input_Using_Float",
+            "Input = Float" """
+        self.assertEqual(
+            classify_triangle(1.1, 2.2, 3.3),
+            "Invalid_Input_Using_Float",
+            "Input = Float",
+        )
+
+
+if __name__ == "__main__":
+    print("Running unit tests")
     unittest.main()
-
-
