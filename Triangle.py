c7f13a4137b00c2702cde5d8f4214cdb38a5b101
douglas0520 <145615003+douglas0520@users.noreply.github.com>
2023-10-14 18:45:49 -0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,60 +1,43 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Jan 14 13:44:00 2016
-Updated Jan 21, 2018
+Module: classify_triangle
 
-The primary goal of this file is to demonstrate a simple python program to classify triangles
+This module defines a function for classifying a triangle based on its side lengths.
 
-@author: jrr
-@author: rk
+Author: Douglas Chi
+Date: October 14, 2023
 """
 
-def classifyTriangle(a,b,c):
+
+def classify_triangle(side_a, side_b, side_c):
     """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
-    This function returns a string with the type of triangle from three integer values
-    corresponding to the lengths of the three sides of the Triangle.
-    
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
-      BEWARE: there may be a bug or two in this code
+    classify_triangle(side_a, side_b, side_c) :  will classify  based on its side inputs.
+
+    side_a : Length of side a.
+    side_b : Length of side b.
+    side_c : Length of side c.
+    Returns: str: The classification of the triangle.
+
     """
 
-    # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput_Reached_Limit'
-        
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput_Reached_Limit'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'Invalid_Input_Using_Float'
-      
-    # This information was not in the requirements spec but 
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    sides = [a,b,c]
-    LongestSide = max(sides)
-    sides.remove(LongestSide)
-    if (LongestSide >= sum(sides)):
-        return 'InvalidInput_Not_A_Triangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == a and a == c:
-        return 'Equilateral'
-    elif (sides[0]**2 + sides[1]**2 == (LongestSide**2)):
-        return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
-    else:
-        return 'Isosceles'
+    if side_a > 200 or side_b > 200 or side_c > 200:
+        return "Invalid_Input_Reached_Limit"
+    if side_a <= 0 or side_b <= 0 or side_c <= 0:
+        return "Invalid_Input_Reached_Limit"
+    if not (
+        isinstance(side_a, int) and isinstance(side_b, int) and isinstance(side_c, int)
+    ):
+        return "Invalid_Input_Using_Float"
+
+    sides = [side_a, side_b, side_c]
+    longest_side = max(sides)
+    sides.remove(longest_side)
+    if longest_side >= sum(sides):
+        return "Invalid_Input_Not_A_Triangle"
+
+    if side_a == side_b and side_b == side_a and side_a == side_c:
+        return "Triangle_Is_Equilateral"
+    elif sides[0] ** 2 + sides[1] ** 2 == (longest_side**2):
+        return "Triangle_Is_Right"
+    elif (side_a != side_b) and (side_b != side_c) and (side_a != side_b):
+        return "Triangle_Is_Scalene"
+    return "Triangle_Is_Isosceles"
